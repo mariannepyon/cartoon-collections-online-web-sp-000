b0b71bab3e934f7884235b870d74157909cf1341
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 20:11:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,9 @@ def roll_call_dwarves(x)# code an argument here
   end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  planeteer_calls.collect {|x| x.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
