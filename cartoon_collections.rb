ae9a9236c8f7ecc2ff94580b3dff1a030ba067a3
Marianne Pyon <mariannepyon@gmail.com>
2019-08-05 19:32:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  new_array = dwarves.split(",")
+  new_array.collect {|x| x.sort}
 end
 
 def summon_captain_planet# code an argument here
